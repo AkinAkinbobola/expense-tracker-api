c42273cb894e141500951f10f263d6c83a496183
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-28 22:23:04 +0100

    Add error handling to signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 import { encodePassword } from '../utils/bcrypt';
@@ -8,6 +8,14 @@ export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async registerUser(data: Prisma.UserCreateInput) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (user) throw new BadRequestException('User already exists');
+
     const { password: rawPassword, ...userDetails } = data;
     const password = await encodePassword(rawPassword);
     return this.prismaService.user.create({
