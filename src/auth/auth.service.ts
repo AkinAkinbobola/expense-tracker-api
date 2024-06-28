a611199bd9e50d72f9636e5817c06a90a53f5a27
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-28 21:12:50 +0100

    Add auth and expenses module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async registerUser() {
+    return "Register Users"
+  }
+
+  async loginUser() {
+    return "Login User"
+  }
+}
