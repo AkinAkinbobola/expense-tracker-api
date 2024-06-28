810468aebe941ab6ccb7b820b61a9b416c9bf5bb
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-28 21:06:01 +0100

    Add auth and expenses module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, ExpensesModule],
   controllers: [],
   providers: [],
 })
